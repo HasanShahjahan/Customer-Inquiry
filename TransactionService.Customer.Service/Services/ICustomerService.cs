2a0c7c8dbf1252f9484d9b8a44b1cd904404b345
DELL <DELL@DESKTOP-BIPRV2L>
2018-12-18 05:05:29 +0600

    Added Customer Interface

---
+++ TransactionService.Customer.Service/Services/ICustomerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransactionService.Customer.Data;
+
+namespace TransactionService.Customer.Service.Services
+{
+    public interface ICustomerService
+    {
+        List<TransactionService.Customer.Data.EntityDataModel.Customer> GetCustomers();
+        TransactionService.Customer.Data.EntityDataModel.Customer GetCustomer(int customerId);
+        TransactionService.Customer.Data.EntityDataModel.Customer GetCustomer(string email);
+        TransactionService.Customer.Data.EntityDataModel.Customer GetCustomer(int customerId,string email);
+    }
+}
