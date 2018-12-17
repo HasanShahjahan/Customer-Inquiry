54b1b688a076e22019552a2d081ace6073540271
DELL <DELL@DESKTOP-BIPRV2L>
2018-12-18 05:18:15 +0600

    Service Layer is ready for get all customer

---
+++ TransactionService.Customer.Service/Services/CustomerService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TransactionService.Customer.Data.EntityDataModel;
+
+
+namespace TransactionService.Customer.Service.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        #region DbContext
+        private TransactionServiceEntities db = new TransactionServiceEntities();
+        #endregion
+
+        #region Service Implementation
+        public Data.EntityDataModel.Customer GetCustomer(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Data.EntityDataModel.Customer GetCustomer(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Data.EntityDataModel.Customer GetCustomer(int customerId, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Data.EntityDataModel.Customer> GetCustomers()
+        {
+            return db.Customers.ToList();
+        }
+        #endregion
+    }
+}
